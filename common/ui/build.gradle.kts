2a799ca93955355b0d727aa0dd42278dae656d56
Onegold11 <ujini1129@gmail.com>
2023-03-18 18:49:44 +0900

    Feat: Add login button
    <description>
    - 소셜 로그인 버튼 추가
    - compose preview 를 위한 라이브러리 추가
    
    <i sue>
    NGOL-21

--- common/ui/build.gradle.kts
+++ common/ui/build.gradle.kts
@@ -12,8 +12,11 @@ dependencies {
     implementation(libs.androidx.compose.compiler)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material.compose)
     implementation(libs.coil)
     implementation(libs.system.ui.controller)
+
+    implementation(project(mapOf("path" to ":common:design-system")))
 }
