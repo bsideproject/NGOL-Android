5584691089ae4263d41e8c9348c303cea203c1bf
Onegold11 <ujini1129@gmail.com>
2023-03-18 18:45:07 +0900

    Feat: Add login button resource
    <description>
    - 소셜 로그인 버튼에 사용할 이미지 리소스 추가
    - 소셜 로그인 버튼에 사용할 색상 데이터 추가
    
    <i sue>
    NGOL-21

--- common/design-system/src/main/java/com/nugu/nuguollim/design_system/theme/Color.kt
+++ common/design-system/src/main/java/com/nugu/nuguollim/design_system/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val KakaoYellow = Color(0xFFFEE500)
+val KakaoTextBlack = Color(0xD9000000)
+val NaverGreen = Color(0xFF03C75A)
