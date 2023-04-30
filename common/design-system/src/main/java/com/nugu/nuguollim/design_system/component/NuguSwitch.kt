42f823aec22a5c6273806eff2ef61907b88355cc
onegold11 <ujini1129@gmail.com>
2023-04-30 12:57:57 +0900

    Feat: Add message edit switch
    <description>
    - 메시지 편집 화면의 스위치 UI 추가
    
    <issue>
    NGOL- 137

---
+++ common/design-system/src/main/java/com/nugu/nuguollim/design_system/component/NuguSwitch.kt
@@ -0,0 +1,125 @@
+package com.nugu.nuguollim.design_system.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nugu.nuguollim.design_system.theme.Gray100
+import com.nugu.nuguollim.design_system.theme.Gray500
+import com.nugu.nuguollim.design_system.theme.Primary500
+import com.nugu.nuguollim.design_system.theme.pretendard
+
+@Composable
+fun NuguSwitch(
+    modifier: Modifier = Modifier,
+    text1: String,
+    text2: String,
+    onChecked: (Boolean) -> Unit = {}
+) {
+    var checked by remember { mutableStateOf(true) }
+
+    Card(
+        shape = RoundedCornerShape(500.dp),
+        modifier = modifier
+            .width(180.dp)
+            .height(34.dp)
+            .clickable {
+                checked = checked.not()
+                onChecked(checked)
+            },
+        elevation = 0.dp,
+    ) {
+        Box(
+            modifier = Modifier.background(Gray100),
+        ) {
+            val checkedText = if (checked) text1 else text2
+            val thumbAlign = if (checked) Alignment.TopStart else Alignment.TopEnd
+
+            NuguSwitchThumb(
+                modifier = Modifier.align(Alignment.TopStart),
+                checked = false,
+                text = text1
+            )
+            NuguSwitchThumb(
+                modifier = Modifier.align(Alignment.TopEnd),
+                checked = false,
+                text = text2
+            )
+            NuguSwitchThumb(
+                modifier = Modifier.align(thumbAlign),
+                checked = true,
+                text = checkedText
+            )
+        }
+    }
+}
+
+@Composable
+fun NuguSwitchThumb(
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    text: String
+) {
+    Card(
+        shape = RoundedCornerShape(500.dp),
+        modifier = modifier
+            .width(95.dp)
+            .height(34.dp),
+        elevation = 0.dp,
+    ) {
+        val backgroundColor = if (checked) Primary500 else Gray100
+        val textColor = if (checked) Color.White else Gray500
+
+        Box(
+            modifier = Modifier.background(backgroundColor),
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier.align(Alignment.Center),
+                color = textColor,
+                fontFamily = pretendard,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 480)
+@Composable
+private fun NuguSwitchPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Gray500),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        NuguSwitch(text1 = "텍스트 추가", text2 = "이미지 편집")
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 480)
+@Composable
+private fun NuguSwitchThumbPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Gray500),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        NuguSwitchThumb(text = "텍스트 추가", checked = false)
+    }
+}
