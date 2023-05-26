67c7d643b02cb33f9ae41022424a1a1e48ab3c10
sun5066 <alstjr4434@naver.com>
2023-05-26 16:07:33 +0900

    Feature: Version Update
    <description>
    - 버전 정보 업데이트
    
    <issue>
    
    1.0.04 -> 1.0.05

--- convention-plugin/convention/src/main/kotlin/com/nugu/nuguollim/plugin/VersionConstants.kt
+++ convention-plugin/convention/src/main/kotlin/com/nugu/nuguollim/plugin/VersionConstants.kt
@@ -5,7 +5,7 @@ object VersionConstants {
     const val MIN_SDK = 24
     const val TARGET_SDK = 33
 
-    const val VERSION_CODE = 4
-    const val VERSION_NAME = "1.0.04"
+    const val VERSION_CODE = 5
+    const val VERSION_NAME = "1.0.05"
 
 }
